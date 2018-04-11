6592537019371209ce0d01997be1576ecae66079
SLLVER <38224735+SLLVER@users.noreply.github.com>
2018-04-11 13:12:30 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-$(document).ready(function(e){
+$(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
@@ -8,10 +8,10 @@ $(document).ready(function(e){
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
 		document.addEventListener("pause",function(){
-			escribehistoria ('la app se pauso');
+			escribehistoria ('la app se pausó');
 		},false);//pause
 		document.addEventListener("resume",function(){
-			escribehistoria ('la app se reinicio');
+			escribehistoria ('la app se reinició');
 		},false);//resume
 		document.addEventListener("online",function(){
 			escribehistoria ('la app se conecto a la red');
